29436b3c55b48835ae44dea1368ee55d250b4eb8
Giovanni Chajon <giochajon@gmail.com>
2019-06-22 15:37:55 -0600

    added first code challenge solution

--- CodingChallenges/src/SpeedConverter.java
+++ CodingChallenges/src/SpeedConverter.java
@@ -4,7 +4,16 @@ public class SpeedConverter {
         if (kilometersPerHour < 0) {
             return -1;
         }
-        return 0;
+        return Math.round(kilometersPerHour / 1.609);
+
     }
 
+    public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+
+    }
 }
