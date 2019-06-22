fb0c194964b702e01661412b59869af524922f0a
Giovanni Chajon <giochajon@gmail.com>
2019-06-22 13:29:44 -0600

    this is a test from IntelliSense

---
+++ CodingChallenges/src/SpeedConverter.java
@@ -0,0 +1,10 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
