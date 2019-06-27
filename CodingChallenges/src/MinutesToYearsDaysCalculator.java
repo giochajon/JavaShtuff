e147570362792f91da4bbac576e59c8f82b288c7
Giovanni Chajon <giochajon@gmail.com>
2019-06-26 19:22:53 -0600

    added number nine (minutes to year and days) code challenge solutions - solution added

--- CodingChallenges/src/MinutesToYearsDaysCalculator.java
+++ CodingChallenges/src/MinutesToYearsDaysCalculator.java
@@ -1,9 +1,17 @@
 public class MinutesToYearsDaysCalculator {
     public static void printYearsAndDays(long minutes){
         if (minutes < 0 ) {
-            System.out.println("Invalid Value");
-
-
+            System.out.println("Invalid Value");}
+        else {
+            long ano = minutes / 525600;
+            long dia = (minutes % 525600) / 1440;
+            System.out.println(minutes + " min = " + ano + " y and " + dia + " d");
         }
+
     }
+
+    public static void main(String[] args) {
+       printYearsAndDays(561600);
+    }
+
 }
