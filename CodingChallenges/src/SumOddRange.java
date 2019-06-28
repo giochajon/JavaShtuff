132d63d6613a51c9304a1f89c5a9676410f8b4f4
Giovanni Chajon <giochajon@gmail.com>
2019-06-27 21:38:52 -0600

    added number fifteen (sum of odds) code challenge solutions -

---
+++ CodingChallenges/src/SumOddRange.java
@@ -0,0 +1,24 @@
+public class SumOddRange {
+
+    public static boolean isOdd (int number){
+        if (number >0 ){
+
+            if (number % 2 !=0){return true;}
+
+        }
+        return false;
+    }
+
+    public static int sumOdd (int start, int end) {
+    int result = 0;
+    if ((end < start )||(start <= 0)) {return -1;}
+
+    for (int i = start ; i<=end ; i++  ) {
+        if (isOdd(i)) {result+=i;}
+
+    }
+    return result;
+    }
+
+}
+
