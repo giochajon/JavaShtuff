a196d2a0647a6da8e6e7349fb9b39e8ed98aba54
Giovanni Chajon <giochajon@gmail.com>
2019-07-02 09:34:47 -0600

    added coding Challenge 18 (shared digit in two pairs of numbers)

--- CodingChallenges/src/sumDigits.java
+++ CodingChallenges/src/sumDigits.java
@@ -78,22 +78,100 @@ public class sumDigits {
 
             // drop the least-significant digit
             number /=10; // same as number = number /10;
-
-
         }
 
         return sum;
+    }
 
+    public static boolean hasSharedDigitDos (int first, int second) {
+        if ((first <= 10 || first >= 99) || (second <= 10 || second >= 99)) {return false;}
 
+        while (first >0 ){
+
+            // extracting least-significant digit
+            int digit = first % 10;
+            if (hasDigit(digit,second) ) {
+                return true;
+            }
+
+            // drop the least-significant digit
+            first /=10; // same as number = number /10;
+        }
+
+        return false;
 
     }
+
+
+    public static boolean hasDigit (int pattern, int number){
+        if ((pattern < 0 || pattern >= 9) ) {return false;}
+
+        while (number >0 ){
+
+            // extracting least-significant digit
+            int digit = number % 10;
+            if (digit  ==pattern) {
+                return true;
+            }
+
+            // drop the least-significant digit
+            number /=10; // same as number = number /10;
+        }
+
+        return false;
+
+    }
+
+
+    public static boolean hasSharedDigitNoSirve (int first, int second) {
+        if ((first <= 10 || first > 99) || (second <= 10 || second > 99)) {return false;}
+
+        while (first >0 ){
+
+            // extracting least-significant digit
+            int digit = first % 10;
+
+            while (second >0 ){
+
+                // extracting least-significant digit
+                int digit2 = second % 10;
+                if (digit2  == digit) {
+                    return true;
+                }
+
+                // drop the least-significant digit
+                second /=10; // same as number = number /10;
+            }
+
+            // drop the least-significant digit
+            first /=10; // same as number = number /10;
+        }
+        return false;
+    }
+
+
+    public static boolean  hasSharedDigit(int first, int second ){
+        if ((first <= 10 || first > 99) || (second <= 10 || second > 99)) {return false;}
+
+        int a = first % 10;
+        int b = first / 10;
+        int c = second % 10;
+        int d = second /10;
+      if (((a - c == 0) || (a - d ==0)) || ((b - c ==0) || (b - d ==0) )) {return true;}
+        //System.out.println(" a "+a +" b "+b +" c "+c  +" d "+d );
+        //System.out.println(b - d ==0);
+      return false;
+    }
+
 
     public static void main(String[] args) {
         //System.out.println("Then sum of the digits in number 125 is "+ sumaDigitos(125));
         //System.out.println("the number 121 is palindrome = " + isPalindrome(121));
         //System.out.println("the number -1221 is palindrome = " + isPalindrome(-1221));
        // System.out.println(sumFirstAndLastDigit(101));
-        System.out.println(getEvenDigitSum(252));
+        //System.out.println(getEvenDigitSum(252));
+        //System.out.println(hasDigit(2,25));
+        System.out.println(hasSharedDigit(12,13));
 
     }
 
