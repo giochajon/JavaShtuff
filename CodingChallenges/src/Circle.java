06dd2df96cd5121e67d1de45a916887769d512ef
Giovanni Chajon <giochajon@gmail.com>
2019-07-22 13:14:46 -0600

    added code challenge 35  (the cylinder p3) added tests

--- CodingChallenges/src/Circle.java
+++ CodingChallenges/src/Circle.java
@@ -3,8 +3,20 @@ public class Circle {
     private double radius;
 
     public Circle(double radius) {
-        this.radius = radius;
-        // 451 con 4
+        if (radius < 0 ) {this.radius = 0;} else
+        {this.radius = radius;}
 
+
+    }
+
+
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return (radius*radius * Math.PI);
     }
 }
